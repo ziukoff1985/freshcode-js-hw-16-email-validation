7eb650d0610e204aacdd184866d0bab1d80551cb
Borys Ziukov <ziukoff1985@gmail.com>
2025-10-07 12:12:42 +0300

    Add general structure

--- src/index.js
+++ src/index.js
@@ -90,155 +90,44 @@ cancelButton.setAttribute('type', 'reset');
 cancelButton.textContent = 'Cancel';
 btnWrapDiv.append(cancelButton);
 
-// DOM - events creation
+// DOM - email validation
+const emailInput = document.querySelector('input[name="email"]');
+console.log(emailInput);
 
-class Person {
-    constructor(...args) {
-        args.forEach(({ name, value }) => {
-            this[name] = value.trim();
-        });
-    }
-}
-
-function onSubmitForm(event) {
-    event.preventDefault();
-    const formInputs = [...document.querySelectorAll('input')].filter(
-        ({ name, value, type }) =>
-            name && value.trim() && type !== 'checkbox' && type !== 'radio'
-    );
-
-    const person = new Person(...formInputs);
-
-    if (!person.lastName) {
-        console.log('Cannot save: Last Name is required and cannot be empty');
-        return;
-    }
-
-    const personJson = JSON.stringify(
-        person,
-        (key, value) =>
-            key === 'password' || key === 'passwordConfirmation'
-                ? undefined
-                : value,
-        2
-    );
-
-    localStorage.setItem(person.lastName, personJson);
-    form.reset();
-}
-
-form.addEventListener('submit', onSubmitForm);
-
-// const form = document.querySelector('form');
-
-// * Version with dynamic props collection
+// // DOM - events creation
 // class Person {
-//     constructor(
-//         firstName = 'Not provided',
-//         lastName,
-//         nickName = 'Not provided',
-//         email = 'Not provided'
-//     ) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.nickName = nickName;
-//         this.email = email;
+//     constructor(...args) {
+//         args.forEach(({ name, value }) => {
+//             this[name] = value.trim();
+//         });
 //     }
 // }
 
 // function onSubmitForm(event) {
 //     event.preventDefault();
-//     const formElements = form.elements;
+//     const formInputs = [...document.querySelectorAll('input')].filter(
+//         ({ name, value, type }) =>
+//             name && value.trim() && type !== 'checkbox' && type !== 'radio'
+//     );
 
-//     const person = new Person();
-
-//     for (const { name, value, tagName } of formElements) {
-//         if (name && tagName === 'INPUT' && value.trim()) {
-//             person[name] = value.trim();
-//         }
-//     }
+//     const person = new Person(...formInputs);
 
 //     if (!person.lastName) {
 //         console.log('Cannot save: Last Name is required and cannot be empty');
 //         return;
 //     }
 
-//     localStorage.setItem(person.lastName, JSON.stringify(person));
+//     const personJson = JSON.stringify(
+//         person,
+//         (key, value) =>
+//             key === 'password' || key === 'passwordConfirmation'
+//                 ? undefined
+//                 : value,
+//         2
+//     );
+
+//     localStorage.setItem(person.lastName, personJson);
 //     form.reset();
 // }
 
 // form.addEventListener('submit', onSubmitForm);
-
-// * Version with event.target.elements
-
-// class Person {
-//     constructor(firstName, lastName, nickName, email) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.nickName = nickName;
-//         this.email = email;
-//     }
-// }
-
-// function onSubmit(event) {
-//     event.preventDefault();
-//     const formElements = event.target.elements;
-
-//     const firstNameValue = formElements.firstName.value.trim();
-//     const lastNameValue = formElements.lastName.value.trim();
-//     const nickNameValue = formElements.nickName.value.trim();
-//     const emailValue = formElements.email.value.trim();
-
-//     if (!lastNameValue) {
-//         console.log('Cannot save: Last Name is required as a key');
-//         form.reset();
-//         return;
-//     }
-
-//     const person = new Person(
-//         firstNameValue || 'Not provided',
-//         lastNameValue || 'Not provided',
-//         nickNameValue || 'Not provided',
-//         emailValue || 'Not provided'
-//     );
-
-//     localStorage.setItem(lastNameValue, JSON.stringify(person));
-
-//     form.reset();
-// }
-
-// form.addEventListener('submit', onSubmit);
-
-// * Version with direct input.value
-
-// const firstName = document.querySelector('input[name="first-name"]');
-// const lastName = document.querySelector('input[name="last-name"]');
-// const nickName = document.querySelector('input[name="nick-name"]');
-// const email = document.querySelector('input[name="email"]');
-
-// class Person {
-//     constructor(firstName, lastName, nickName, email) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.nickName = nickName;
-//         this.email = email;
-//     }
-// }
-
-// function onSubmit(event) {
-//     event.preventDefault();
-//     const firstNameValue = firstName.value || 'Value does not provided';
-//     const lastNameValue = lastName.value || 'Value does not provided';
-//     const nickNameValue = nickName.value || 'Value does not provided';
-//     const emailValue = email.value || 'Value does not provided';
-
-//     localStorage.setItem(
-//         lastNameValue,
-//         JSON.stringify(
-//             new Person(firstNameValue, lastNameValue, nickNameValue, emailValue)
-//         )
-//     );
-// }
-
-// const buttonSubmit = document.querySelector('button[type="submit"]');
-// buttonSubmit.addEventListener('click', onSubmit);
