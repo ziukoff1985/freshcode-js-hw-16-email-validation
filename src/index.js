cadbb39e41a01e175c8a69b1aca940ac2cacc234
Borys Ziukov <ziukoff1985@gmail.com>
2025-10-08 13:34:57 +0300

    Change the logic from the file index.js into separate files: add submitForm.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 import { createFormElements } from './createForm.js';
+import { setupSubmitForm } from './submitForm.js';
 import { setupEmailValidation } from './validateEmail.js';
 
 createFormElements();
+
 setupEmailValidation('input[name="email"]');
+
+setupSubmitForm();
 
 // import { inputConfigData, radioConfigData } from './configData.js';
 
